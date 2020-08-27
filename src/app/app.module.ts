aee093db8f6bf81da93500b81d1fabe13551b940
Raja-sekhar533 <rajasekharlovly.k@gmail.com>
2020-08-27 15:37:57 +0530

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import { WeatherComponent } from './weather/weather.component'
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ],
+  declarations: [ AppComponent, HelloComponent,WeatherComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
