2e3a70f4c4b9447797c7d5bf00200784f534ea0d
Raja-sekhar533 <rajasekharlovly.k@gmail.com>
2020-08-28 06:21:33 +0530

    2 binaries uploaded

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,10 +10,16 @@ lat;
 lon;
 weather;
 temp_min ;
+isday ;
+
   constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
     //this.weatherService.getWeatherDataByCordinates(35, 139).subscribe(console.log)
+    this.weather = {
+      main: {},
+      isDay:true
+    }
     this.getLocation();
   }
 getLocation(){
@@ -21,10 +27,11 @@ getLocation(){
     navigator.geolocation.watchPosition((success)=>{
       this.lat = success.coords.latitude;
       this.lon = success.coords.longitude;
-
+let currentDate = new Date;
+this.isday = (currentDate.getTime()< this.weather.sunsetTime.getTime());
       this.weatherService.getWeatherDataByCordinates(this.lat, this.lon).subscribe(data => {
         this.weather = data;
-        
+        this.setWeatherData(data);
       })
     })
   }
@@ -33,5 +40,14 @@ getCity(city){
 this.weatherService.getWeatherDataByCityName(city).subscribe(data => {
   this.weather = data;
 })
+console.log(this.weather.isDay)
+}
+setWeatherData(data){
+  this.weather = data;
+    let sunsetTime = new Date(this.weather.sys.sunset * 1000);
+    this.weather.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.weather.isDay = (currentDate.getTime() < sunsetTime.getTime());
+
 }
 }
